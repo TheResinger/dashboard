f5153147e7ed7025f9d589859b0bfc7288ad3b18
TheResinger <morrowbenjamin42@gmail.com>
2024-06-10 16:54:32 -0400

    fixed bg color

--- src/app/_components/Dashboard.tsx
+++ src/app/_components/Dashboard.tsx
@@ -7,7 +7,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 
 export default function Dashboard() {
     return (
-        <div className="flex-1 space-y-4 py-8 pt-6">
+        <div className="container flex-1 space-y-4 py-8 pt-6">
             <div className="flex items-center justify-between space-y-2">
                 <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
             </div>
